1c6659a016cb847cca27d7d7e55a8c757f939b42
Kazuyoshi Kato <kato.kazuyoshi@gmail.com>
2018-05-18 20:47:43 -0700

    Try to implement v2 in Nuxt

--- pages/_me.vue
+++ pages/_me.vue
@@ -1,29 +1,28 @@
 <template>
-    <div>
-<h1>
-  <div>
-    <span class="given">Kazuyoshi</span>
-    <span class="family">Kato</span>
-  </div>
-  <div>
-    <span class="family">加藤</span>
-    <span class="given">和良</span>
-  </div>
-</h1>
+    <div id="container">
+      <h1>Kazuyoshi Kato / 加藤 和良</h1>
+      <p>
+        I'm a software developer, Japanese and Seattle transplant.
+      </p>
 
-<p>
-  I'm a software developer, Japanese and Seattle transplant.
-</p>
-
-<p>
-  I work as a Software Development Engineer at <a href="https://www.amazon.com/">Amazon.com</a>.
-  I joined Amazon Japan in 2012, and moved to Seattle in 2014.
-  Before Amazon, I worked for <a href="https://mixi.co.jp">Mixi</a>.
-  The company is now known for mobile games, but I was working on
-  its social networking service.
-</p>
-<p>
-  <em>But</em>, all opinions expressed here are entirely my own.
-</p>
+      <p>
+        I work as a Software Development Engineer at <a href="https://www.amazon.com/">Amazon.com</a>.
+        I joined Amazon Japan in 2012, and moved to Seattle in 2014.
+        Before Amazon, I worked for <a href="https://mixi.co.jp">Mixi</a>.
+        The company is now known for mobile games, but I was working on
+        its social networking service.
+      </p>
+      <p>
+        <em>But</em>, all opinions expressed here are entirely my own.
+      </p>
     </div>
 </template>
+<script>
+    export default {
+        head() {
+            return {
+                title: 'Kazuyoshi Kato / 加藤 和良',
+            }
+        }
+    }
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,29 @@
 <template>
     <div>
-        <a href="/me/">me</a>
+        <header>
+            <div class="content">
+                <nuxt-link to="/">8-p.info</nuxt-link>
+                <div id="menu">
+                    <nuxt-link to="/me/">Kazuyoshi Kato</nuxt-link>
+                </div>
+            </div>
+        </header>
+        <div id="container">
+            <div id="hello">
+                <blockquote>
+                    <p>This led me to suggest that the null value is a member of every type, and a null check is required on every use of that reference variable, and it may be perhaps a billion dollar mistake.</p>
+                    <footer><a href="https://www.infoq.com/presentations/Null-References-The-Billion-Dollar-Mistake-Tony-Hoare">Tony Hoare</a></footer>
+                </blockquote>
+            </div>
+        </div>
     </div>
 </template>
+<script>
+    export default {
+        head() {
+            return {
+                title: '8-p.info'
+            }
+        }
+    }
+</script>
