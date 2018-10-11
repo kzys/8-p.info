1ff04ab7e44b398e1363d050ab33de068c8fb2c5
Kazuyoshi Kato <kato.kazuyoshi@gmail.com>
2018-10-11 00:42:30 -0700

    This Year

---
+++ pages/this-year.vue
@@ -0,0 +1,44 @@
+<style>
+h1 { font-size: 150% }
+.bar {
+    border: 1px solid #000;
+}
+.bar .inner {
+    background: #000;
+}
+</style>
+<template>
+    <div>
+        <div id="hello">
+            <div class="content">
+                <h1>{{ message }}</h1>
+                <div class="bar"><div class="inner" v-bind:style="{ width: barWidth }">&nbsp;</div></div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+    import Header from '@/components/Header.vue'
+    export default {
+        head: {
+            title: 'This Year'
+        },
+        mounted() {
+            let d = new Date()
+            let firstDay = new Date(d.getFullYear(), 0, 1);
+            let lastDay = new Date(d.getFullYear()+1, 0, 1);
+            window.setInterval(() => {
+                let elapsed = d - firstDay;
+                let duration = lastDay - firstDay;
+                let percentage = elapsed / duration * 100;
+
+                this.message = `This year, ${ d.getFullYear() } is ${percentage.toFixed(2)}% complete.`;
+                this.barWidth = percentage + '%';
+            }, 1000)
+        },
+        data() {
+            return { message: '', barWidth: '0%' };
+        },
+        components: { Header }
+    }
+</script>
