11b036fe6c36be4a25fa179a786c663143d48dd9
Kazuyoshi Kato <kato.kazuyoshi@gmail.com>
2018-10-25 22:29:32 -0700

    I dislike gatekeepers

--- pages/index.vue
+++ pages/index.vue
@@ -4,12 +4,8 @@
         <div id="hello">
             <div class="content">
                 <blockquote>
-                    <p>We're against complexity. We believe designing systems is a fight against
-                    complexity. We'll accept to fight the complexity when it's worthwhile but
-                    we'll try hard to recognize when a small feature is not worth 1000s of lines
-                    of code. Most of the time the best way to fight complexity is by not
-                    creating it at all.</p>
-                    <footer><a href="https://github.com/antirez/redis/blob/unstable/MANIFESTO">The Redis Manifesto</a></footer>
+                    <p>I am emphasizing the self-service nature of these platforms because it’s important for a reason I think is somewhat non-obvious: even well-meaning gatekeepers slow innovation. When a platform is self-service, even the improbable ideas can get tried, because there’s no expert gatekeeper ready to say “that will never work!” And guess what – many of those improbable ideas do work, and society is the beneficiary of that diversity.</p>
+                    <footer><a href="https://www.sec.gov/Archives/edgar/data/1018724/000119312512161812/d329990dex991.htm">Jeff Bezos</a></footer>
                 </blockquote>
             </div>
         </div>
