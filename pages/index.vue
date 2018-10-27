558cabfe55b708f402630082b20e1446caca23d7
Kazuyoshi Kato <kato.kazuyoshi@gmail.com>
2018-10-27 08:02:14 -0700

    Add "This Year" on /

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,17 @@
 <template>
-    <div>
+    <div id="index">
         <Header/>
-        <div id="hello">
+        <div id="quote">
             <div class="content">
                 <blockquote>
                     <p>I am emphasizing the self-service nature of these platforms because it’s important for a reason I think is somewhat non-obvious: even well-meaning gatekeepers slow innovation. When a platform is self-service, even the improbable ideas can get tried, because there’s no expert gatekeeper ready to say “that will never work!” And guess what – many of those improbable ideas do work, and society is the beneficiary of that diversity.</p>
                     <footer><a href="https://www.sec.gov/Archives/edgar/data/1018724/000119312512161812/d329990dex991.htm">Jeff Bezos</a></footer>
                 </blockquote>
+            </div>
+        </div>
+        <div id="experiments">
+            <div class="content">
+                <nuxt-link to="/this-year/">This Year</nuxt-link>
             </div>
         </div>
     </div>
