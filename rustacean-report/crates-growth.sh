0299012924425db2154b0da7372f14c6bcc735dc
Kazuyoshi Kato <kato.kazuyoshi@gmail.com>
2018-12-12 00:07:00 -0800

    Don't include .git directory

--- rustacean-report/crates-growth.sh
+++ rustacean-report/crates-growth.sh
@@ -5,7 +5,6 @@ for rev in $(python ../../daily-snapshot.py "$@" | cut -d ' ' -f2)
 do
     git checkout $rev
     git log -1 \
-        "--pretty=format:%aI,%h,$(find . -type f | wc -l | tr -d ' ')"
+        "--pretty=format:%aI,%h,$(find . -not -path '*/\.*' -type f | wc -l | tr -d ' ')"
     echo
 done
-
