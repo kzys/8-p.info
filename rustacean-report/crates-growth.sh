70054a592c2b5f6bda82389cb31d220e703561f9
Kazuyoshi Kato <kato.kazuyoshi@gmail.com>
2018-12-13 22:12:12 -0800

    Don't checkout files

--- rustacean-report/crates-growth.sh
+++ rustacean-report/crates-growth.sh
@@ -1,10 +1,9 @@
 #! /bin/sh
 set -eu
 
-for rev in $(python ../../daily-snapshot.py "$@" | cut -d ' ' -f2)
+for rev in $(python daily-snapshot.py "$@" | cut -d ' ' -f2)
 do
-    git checkout $rev
-    git log -1 \
-        "--pretty=format:%aI,%h,$(find . -not -path '*/\.*' -type f | wc -l | tr -d ' ')"
+    count=$(git ls-tree --full-tree -r "$rev" | wc -l | tr -d ' ')
+    git log -1 --pretty=format:%aI,%h,$count "$rev"
     echo
 done
