720ad197d2d5b58fe64ff54280b1df195060a060
Kazuyoshi Kato <kato.kazuyoshi@gmail.com>
2018-12-14 00:06:56 -0800

    Use _visualizing-crates-io correctly

--- visualizing-crates-io/vue/pages/visualizing-crates-io.vue
+++ visualizing-crates-io/vue/pages/visualizing-crates-io.vue
@@ -87,7 +87,7 @@
             },
 
             async renderPackageCount() {
-                let res = await axios.get(`package-count.json`);
+                let res = await axios.get(`/_visualizing-crates-io/package-count.json`);
                 var el = this.getCanvas('package-count');
 
                 let data = res.data;
@@ -111,7 +111,7 @@
             },
 
             async renderPopularPackages(kind) {
-                let res = await axios.get(`popular-${kind}-packages.json`);
+                let res = await axios.get(`/_visualizing-crates-io/popular-${kind}-packages.json`);
                 var el = this.getCanvas(`popular-${kind}-packages`);
 
                 let labels = res.data.map(x => x.index)
