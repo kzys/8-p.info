16c0043169fe99a1e17ec95e3f22834f5d5d8b9f
Kazuyoshi Kato <kato.kazuyoshi@gmail.com>
2018-12-20 22:38:36 -0800

    Use Vega-Lite instead

--- visualizing-crates-io/vue/pages/visualizing-crates-io.vue
+++ visualizing-crates-io/vue/pages/visualizing-crates-io.vue
@@ -15,7 +15,7 @@
             <p>
                 In October 2018, the number of crates on the registry exceeded 20,000.
             </p>
-            <div class="chart" id="package-count"><canvas/></div>
+            <div class="chart" id="package-count"><div/></div>
             <p>
                 Another interesting moment was happening in September.
                 Due to the growth, the Rust team decided to
@@ -29,15 +29,15 @@
                 While there are a lot of crates out there, 
                 many crates have only one or two releases.
                 The ecosystem is still faily young.
-                This histogram shows the distribution of the number of releases from 1 to 20.
+                This histogram shows the distribution of the number of releases.
             </p>
-            <div class="chart" id="package-age"><canvas/></div>
+            <div class="chart" id="package-age"><div/></div>
             <p>
                 On the other hand, 1.43% of crates have more than 100 releases.
                 Crates with "rustc-ap-" prefix are automatically published by
                 <a href="https://github.com/alexcrichton/rustc-auto-publish">alexcrichton/rustc-auto-publish</a> though.
             </p>
-            <div class="chart" id="mature-packages"><canvas/></div>
+            <div class="chart" id="mature-packages"><div/></div>
 
             <h2>Dependencies</h2>
             <p>
@@ -69,15 +69,15 @@
             <div class="row">
                 <div class="col">
                     <h3>Normal</h3>
-                    <div id="popular-normal-packages"><canvas/></div>
+                    <div id="popular-normal-packages"><div/></div>
                 </div>
                 <div class="col">
                     <h3>Development</h3>
-                    <div id="popular-dev-packages"><canvas/></div>
+                    <div id="popular-dev-packages"><div/></div>
                 </div>
                 <div class="col">
                     <h3>Build</h3>
-                    <div id="popular-build-packages"><canvas/></div>
+                    <div id="popular-build-packages"><div/></div>
                 </div>
             </div>
         </div>
@@ -93,6 +93,7 @@
 <script>
     import Chart from 'chart.js';
     import axios from 'axios';
+    import vegaEmbed from 'vega-embed';
 
     const AXIS = {
         ticks: {
@@ -113,11 +114,11 @@
         async mounted() {
             this.renderPackageCount();
             this.renderPackageAge();
-            this.renderMaturePackages();
+            // this.renderMaturePackages();
             this.renderPopularPackages('normal');
             this.renderPopularPackages('dev');
             this.renderPopularPackages('build');
-        },
+          },
 
         data() {
             return { };
@@ -128,144 +129,119 @@
             },
 
             async renderPackageCount() {
-                let res = await axios.get(`/_visualizing-crates-io/package-count.json`);
-                var el = this.getCanvas('package-count');
-
-                let data = res.data;
-                let labels = data.map(x => x.author_date_time)
-                let values = data.map(x => x.package_count)
-
-                var myChart = new Chart(el, {
-                    type: 'line',
-                    data: {
-                        labels: labels,
-                        datasets: [{
-                            label: '# of packages',
-                            data: values,
-                            borderColor: 'rgba(227,74,51,0.6)',
-                            fill: false,
-                        }],
+                let spec = {
+                    width: 800,
+                    height: 400,
+                    "$schema": "https://vega.github.io/schema/vega-lite/v3.json",
+                    "description": "A simple bar chart with embedded data.",
+                    "data": {
+                        url: '/_visualizing-crates-io/package-count.json',
                     },
-                    options: {
-                        maintainAspectRatio: false,
-                        legend: { display: false },
-                        scales: {
-                            yAxes: [ AXIS ]
+                    "mark": {type: "line" },
+                    "encoding": {
+                        "x": {
+                            field: "author_date_time",
+                            type: "temporal",
+                            title: "Date",
                         },
-                        tooltips: {
-                            callbacks: {
-                                label: function(item, data) {
-                                    return item.yLabel.toLocaleString();
-                                }
-                            }
-                        }
-                    },
-                });            
-            },
-
-            async renderPopularPackages(kind) {
-                let res = await axios.get(`/_visualizing-crates-io/popular-${kind}-packages.json`);
-                var el = this.getCanvas(`popular-${kind}-packages`);
-
-                let labels = res.data.map(x => x.index)
-                let data = res.data.map(x => x.count)
-
-                var myChart = new Chart(el, {
-                    type: 'horizontalBar',
-                    data: {
-                        labels: labels,
-                        datasets: [{
-                            label: '# of dependents',
-                            data: data,
-                            backgroundColor: 'rgba(227,74,51,0.6)',
-                            borderWidth: 0,
-                        }]
-                    },
-                    options: {
-                        maintainAspectRatio: false,
-                        legend: { display: false },
-                        scales: {
-                            xAxes: [ AXIS ]
-                        },
-                        tooltips: {
-                            callbacks: {
-                                label: function(item, data) {
-                                    return item.xLabel.toLocaleString();
-                                }
-                            }
+                        "y": {
+                            field: "package_count",
+                            type: "quantitative",
+                            title: 'Number of Crates',
+                            scale: { domain: [12000, 22000] }
                         }
                     }
-                });            
+                };
+                vegaEmbed('#package-count div', spec, {actions: false});
             },
 
             async renderPackageAge() {
-                let res = await axios.get(`/_visualizing-crates-io/package-age.json`);
-                var el = this.getCanvas(`package-age`);
-
-                let labels = [...Array(res.data['revisions'].length).keys()].map(x => x+1);
-
-                var myChart = new Chart(el, {
-                    type: 'bar',
-                    data: {
-                        labels: labels,
-                        datasets: [
-                            {
-                            label: '# of revisions',
-                            data: res.data['revisions'],
-                            backgroundColor: 'rgba(227,74,51,0.6)',
-                            borderWidth: 0,
-                        }
-                        ]
+                let spec = {
+                    width: 800,
+                    height: 400,
+                    "$schema": "https://vega.github.io/schema/vega-lite/v3.json",
+                    "data": {
+                        url: '/_visualizing-crates-io/package-revisions.json',
                     },
-                    options: {
-                        maintainAspectRatio: false,
-                        legend: { display: false },
-                        scales: {
-                            yAxes: [ AXIS ],
-                            xAxes: [ AXIS ],
+                    "encoding": {
+                        "x": {
+                            field: "revisions",
+                            type: "quantitative",
+                            title: "Number of Releases",
+                            bin: { maxbins: 50 },
                         },
-                        tooltips: {
-                            callbacks: {
-                                label: function(item, data) {
-                                    return item.yLabel.toLocaleString();
-                                }
-                            }
+                        "y": {
+                            aggregate: "count",
+                            type: "quantitative",
+                            title: 'Number of Crates',
+                            scale: {"type": "log"},
                         }
-                    }
-                });            
+                    },
+                    layer: [ { mark: 'bar' } ]
+                };
+                vegaEmbed('#package-age div', spec, {actions: false});
             },
 
-            async renderMaturePackages() {
-                let res = await axios.get(`/_visualizing-crates-io/mature-packages.json`);
-                var el = this.getCanvas(`mature-packages`);
-
-                let labels = res.data.map(x => x.package)
-                let data = res.data.map(x => x.revisions)
-
-                var myChart = new Chart(el, {
-                    type: 'horizontalBar',
-                    data: {
-                        labels: labels,
-                        datasets: [{
-                            label: '# of versions',
-                            data: data,
-                            backgroundColor: 'rgba(227,74,51,0.6)',
-                            borderWidth: 0,
-                        }]
+            async renderMaturePackages0() {
+                let spec = {
+                    width: 400,
+                    height: 600,
+                    "$schema": "https://vega.github.io/schema/vega-lite/v3.json",
+                    "data": {
+                        url: '/_visualizing-crates-io/all-packages.json',
                     },
-                    options: {
-                        maintainAspectRatio: false,
-                        legend: { display: false },
-                        scales: { xAxes: [ AXIS ] },
-                        tooltips: {
-                            callbacks: {
-                                label: function(item, data) {
-                                    return item.xLabel.toLocaleString();
-                                }
-                            }
+                    transform: [
+                        {"filter": {"field": "revisions", "gt": 100}},
+                        {"sample": 10},
+                    ],
+                    "encoding": {
+                        "x": {
+                            field: "revisions",
+                            type: "quantitative",
+                            title: "Number of Releases",
+                        },
+                        "y": {
+                            field: "package",
+                            type: "ordinal",
+                            sort: {field: 'revisions', op:'max',order:'descending'},
+                            title: 'Name'
                         }
-                    }
-                });            
+                    },
+                    layer: [ { mark: 'bar' } ]
+                };
+                vegaEmbed('#mature-packages div', spec, {actions: false});
+            },
+
+            async renderPopularPackages(kind) {
+                let key = 'clients_count_' + kind;
+                let spec = {
+                    width: 200,
+                    height: 400,
+                    $schema: "https://vega.github.io/schema/vega-lite/v3.json",
+                    data: { url: '/_visualizing-crates-io/popular-packages.json' },
+                    transform: [
+                        {
+                            window: [{ op: 'rank', as: 'rank' }],
+                            sort: [{ field: key, order: 'descending' }]
+                        },
+                        { filter: 'datum.rank <= 20' },
+                    ],
+                    encoding: {
+                        x: {
+                            field: key,
+                            type: "quantitative",
+                            title: "Number of Dependents",
+                        },
+                        y: {
+                            field: "index",
+                            type: "ordinal",
+                            sort: { field: key, op: 'max', order: 'descending' },
+                            title: 'Name'
+                        }
+                    },
+                    layer: [ { mark: 'bar' } ],
+                };
+                vegaEmbed(`#popular-${kind}-packages`, spec, {actions: false});
             },
 
             async renderDependencyHistogram() {
@@ -359,25 +335,11 @@ p {
     justify-content: space-between;
 }
 
-#package-count {
-    height: 400px;
-}
-
 .chart {
-    margin: 0 auto;
+    text-align: center;
 }
 
-#package-age {
-    width: 600px;
-    height: 200px;
-}
 
-#mature-packages {
-    width: 800px;
-    height: 200px;
-}
-
-#mature-packages,
 #popular-normal-packages,
 #popular-dev-packages,
 #popular-build-packages {
