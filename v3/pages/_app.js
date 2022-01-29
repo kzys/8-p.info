98824dce58e6341c659b8e03b47083047c7a62fc
Kazuyoshi Kato <kato.kazuyoshi@gmail.com>
2022-01-28 18:52:09 -0800

    Not all components are functions

--- v3/pages/_app.js
+++ v3/pages/_app.js
@@ -2,6 +2,6 @@ import '../global.css'
 import Layout from '../lib/layout'
 
 export default function MyApp({ Component, pageProps }) {
-    let c = Component(pageProps)
-    return <Layout title={c.props.title}>{c}</Layout>
+    let c = <Component {...pageProps} />
+    return <Layout title={c.props.title || ''}>{c}</Layout>
 }
