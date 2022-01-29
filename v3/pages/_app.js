d7a14e8ff40d418f882072c6fe77b9ebd112be49
Kazuyoshi Kato <kato.kazuyoshi@gmail.com>
2022-01-29 01:53:32 -0800

    Fix title

--- v3/pages/_app.js
+++ v3/pages/_app.js
@@ -1,7 +1,20 @@
 import '../global.css'
+import Link from 'next/link'
 import Layout from '../lib/layout'
 
 export default function MyApp({ Component, pageProps }) {
-    let c = <Component {...pageProps} />
-    return <Layout title={c.props.title || ''}>{c}</Layout>
+    var c;
+    try {
+        // This one has props.
+        c = Component(pageProps);
+    } catch {
+        // This one doesn't have props...
+        c = <Component {...pageProps} />
+    }
+    return <Layout title={c.props.title || ''}>
+        {c}
+        <footer style={{ borderTop: '1px solid #000', paddingTop: '1rem' }}>
+            <Link href="/">8-p.info</Link>
+        </footer>
+    </Layout>
 }
