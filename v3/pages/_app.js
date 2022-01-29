aafc41fa278009248b9b50028ec7192d647a207a
Kazuyoshi Kato <kato.kazuyoshi@gmail.com>
2022-01-28 18:36:21 -0800

    Move /now/ to Next

--- v3/pages/_app.js
+++ v3/pages/_app.js
@@ -1,5 +1,7 @@
 import '../global.css'
+import Layout from '../lib/layout'
 
 export default function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />
+    let c = Component(pageProps)
+    return <Layout title={c.props.title}>{c}</Layout>
 }
